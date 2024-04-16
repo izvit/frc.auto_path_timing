2652822d517ea7ff3d4cd1e6a7a82126a86a18ec
Vitaliy Gleyzer <vitaliy.gleyzer@littletonrobotics.org>
2024-04-15 21:58:59 -0400

    Successfully generated table from API

--- src/routes/api/bluealliance/+server.js
+++ src/routes/api/bluealliance/+server.js
@@ -1,6 +1,17 @@
 import { json } from '@sveltejs/kit';
 import { JSDOM } from 'jsdom';
 
+function getFirstLink(elem, prefix) {
+    let vidLinks =elem.getElementsByTagName("a")
+    let vidLink = null
+    if (vidLinks.length > 0) 
+        vidLink = vidLinks[0].getAttribute("href")
+        if (!vidLink.startsWith(prefix))
+            return null
+        return vidLinks[0]
+    return null
+}
+
 export async function POST({ cookies, request }) {
     //-- Parse form
     const {eventName} = await request.json()
@@ -10,7 +21,7 @@ export async function POST({ cookies, request }) {
    
     let uri ="https://www.thebluealliance.com/event/" + eventName
     console.info("Fetching: " + uri)
-    var matchTableHtml = await fetch(uri)
+    var resultsArr = await fetch(uri)
         .then(function(response) {
             return response.text()
         })
@@ -19,21 +30,49 @@ export async function POST({ cookies, request }) {
             //console.log(dom.window.document.body.innerHTML)
             let matchTable =  dom.window.document.getElementById("qual-match-table")
             console.log("Match table len: " + matchTable.innerHTML.length)
-            matchTableHtml = matchTable.outerHTML
-            // for(let i=0; i<matchTable.rows.length; i++){
-                
-            // 	row = {
-                    
-            // 	}
-            // }
-            return matchTableHtml
+
+            const resultsArr = []
+            let tbody = matchTable.getElementsByTagName("tbody")[0];
+            let rows = tbody.getElementsByClassName("visible-lg")
+            for(let i=0; i<rows.length; i++){
+                console.log("link" + getFirstLink(rows[i].cells[2], "/team").text)
+                resultsArr.push({
+                    "videoUrl" :  getFirstLink(rows[i].cells[0], "/match")?.getAttribute("href"),
+                    "red0" :  { number : getFirstLink(rows[i].cells[2], "/team")?.text,
+                    uri :    getFirstLink(rows[i].cells[2], "/team")?.getAttribute("href")
+                    },
+                    "red1" :  { number : getFirstLink(rows[i].cells[3], "/team")?.text,
+                    uri :    getFirstLink(rows[i].cells[3], "/team")?.getAttribute("href")
+                    },
+                    "red2" :  { number : getFirstLink(rows[i].cells[4], "/team")?.text,
+                    uri :    getFirstLink(rows[i].cells[4], "/team")?.getAttribute("href")
+                    },
+                    "blue0" :  { number : getFirstLink(rows[i].cells[5], "/team")?.text,
+                    uri :    getFirstLink(rows[i].cells[5], "/team")?.getAttribute("href")
+                    },
+                    "blue1" :  { number : getFirstLink(rows[i].cells[6], "/team")?.text,
+                    uri :    getFirstLink(rows[i].cells[6], "/team")?.getAttribute("href")
+                    },
+                    "blue2" :  { number : getFirstLink(rows[i].cells[7], "/team")?.text,
+                    uri :    getFirstLink(rows[i].cells[7], "/team")?.getAttribute("href")
+                    }
+                })
+            }
+
+            return resultsArr
         })
         .catch(function(err) {  
             console.log('Failed to fetch page: ', err);  
         });
 
-        
-    console.log("Returning response: " + matchTableHtml.length)
-    return new Response( matchTableHtml, {status: 200} )
+    FileSystemWritableFileStream  
+    console.log("Returning rows: " + resultsArr.length)
+    return new Response( JSON.stringify(resultsArr), 
+                        {
+                            status: 200,
+                            headers: new Headers({
+                                "content-type": "application/json",
+                            }),
+                        })
 
 }
--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	import { onMount } from "svelte";
-	let table;
+	
+	let resp;
+	let rows;
+
+	$: if(resp) rows = resp
+
 
 </script>
 <svelte:head>
@@ -8,22 +13,16 @@
 	<meta name="description" content="Match Explorer" />
 </svelte:head>
 
-<!-- <form method="POST" action="getMatchInfo">
-	<label class="text-yellow-200">
-		Event Name: 
-		<input name="eventName" type="text">
-	</label>
-</form> -->
-
 <input
 	type="text"
+	value="2023hop"
 	on:keydown={async (e) => {
 		if (e.key !== 'Enter') return;
 
 		const input = e.currentTarget;
 		const eventName = input.value;
 
-		const response = await fetch('/api/bluealliance', {
+		let response = await fetch('/api/bluealliance', {
 			method: 'POST',
 			body: JSON.stringify({ eventName }),
 			headers: {
@@ -31,9 +30,54 @@
 			}
 		});
 
-		table.innerHTML = await response.text()
+		resp = await response.json()
+		console.log(resp)
 		input.value = ''
 	}}
 />
-<div bind:this={table}> 
-</div>
+
+
+{#if (rows!=null)}
+	<table class="bg-white">
+		<thead>
+			<tr>
+				<th>Video</th>
+				<th>Team0</th>
+				<th>Team1</th>
+				<th>Team2</th>
+				<th>Team0</th>
+				<th>Team1</th>
+				<th>Team2</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each rows as {videoUrl, red0, red1, red2, blue0, blue1, blue2}}
+				<tr>
+					<td>
+						<a href="{videoUrl}" class="font-medium text-yellow-600 dark:text-yellow-500 hover:underline">Video</a>
+					</td>
+					<td>
+						<a href="{red0.uri}" class="font-medium text-red-600 dark:text-red-500 hover:underline">{red0.number}</a>
+					</td>
+					<td>
+						<a href="{red1.uri}" class="font-medium text-red-600 dark:text-red-500 hover:underline">{red1.number}</a>
+					</td>
+					<td>
+						<a href="{red2.uri}" class="font-medium text-red-600 dark:text-red-500 hover:underline">{red2.number}</a>
+					</td>
+					<td>
+						<a href="{blue0.uri}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{blue0.number}</a>
+					</td>
+					<td>
+						<a href="{blue1.uri}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{blue0.number}</a>
+					</td>	
+					<td>
+						<a href="{blue2.uri}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{blue0.number}</a>
+					</td>						
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{/if}
+
+
